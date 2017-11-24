36341c02253947dd6e8309469391b724407be2ad
Chandan <chandank.kumar@gmail.com>
2017-11-24 18:03:29 -0500

    Update gauge-example.py
    Added simple loop

--- gauge-example.py
+++ gauge-example.py
@@ -4,7 +4,11 @@ from prometheus_client import Gauge
 def main():
     g = Gauge('bcr_gauge_example', 'Testing how Prometheus Gauge works')   
     start_http_server(8000)
-    g.set(1)
+    while True:
+      g.inc(3)
+      time.sleep(5)
+      g.dec(2)
+
     
       
 if __name__ == "__main__":
