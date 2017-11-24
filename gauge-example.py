70e0381a9963832e88c2411d57ad173eb9953f59
Chandan <chandank.kumar@gmail.com>
2017-11-24 17:09:21 -0500

    Update gauge-example.py
    Fixed the variable name

--- gauge-example.py
+++ gauge-example.py
@@ -2,7 +2,7 @@ from prometheus_client import start_http_server, Summary
 from prometheus_client import Gauge
 
 def main():
-    g = Gauge('bcr-gauge-example', 'Testing how Prometheus Gauge works')   
+    g = Gauge('bcr_gauge_example', 'Testing how Prometheus Gauge works')   
     start_http_server(8000)
     g.set(1)
     
