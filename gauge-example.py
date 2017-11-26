045573cd1fe0ca0a3c17ecc6a06f06173529ca30
Chandan Kumar <chandank@becloudready.com>
2017-11-25 22:36:04 -0500

    Added running the python script

--- gauge-example.py
+++ gauge-example.py
@@ -1,5 +1,6 @@
 from prometheus_client import start_http_server, Summary
 from prometheus_client import Gauge
+import time
 
 def main():
     g = Gauge('bcr_gauge_example', 'Testing how Prometheus Gauge works')   
